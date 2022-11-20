ff174429469c0336876378b8020a1721a720620f
Motiur Rahman <motiur.cmt@gmail.com>
2022-11-20 18:20:28 +0600

    update

--- ngenit/js/nasted.tab.js
+++ ngenit/js/nasted.tab.js
@@ -134,3 +134,15 @@ var tabbis = (function() {
   
     return fn;
   })();
+
+  //---Inner Page code
+  var tabs = tabbis.init({
+    tabGroup: "[data-tabs]",
+    paneGroup: "[data-panes]",
+    tabActive: "active",
+    paneActive: "active",
+    callback: function(tab, pane) {
+      console.log(tab);
+      console.log(pane);
+    }
+  });
