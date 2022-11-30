4122f7e1bfe1d34a3c4edf90c4fe11f1210273b1
Motiur Rahman <motiur.cmt@gmail.com>
2022-11-30 15:50:40 +0600

    update 30-11-2022

--- document/Testing/js/javascript.mr.js
+++ document/Testing/js/javascript.mr.js
@@ -1,3 +1,99 @@
+/*================/// Nav Menu Style page ///==============*/
+//--Menu Open & hide
+function fnClick(e){
+    var $dd = $(this).next();
+    console.log($dd, this); 
+    $('.manege_nav').not($dd).removeClass('menu_btn_active');
+    $dd.toggleClass('menu_btn_active');
+};
+
+//open menu
+$('.country-btn-portugal').click(fnClick);
+$('.country-btn-uk').click(fnClick);
+
+//close if menu clicked
+$('#mclose').click(()=>{ 
+  $('.manege_nav').removeClass('menu_btn_active');
+});
+
+//--Mobile Menu
+function switchMenuVisible() {
+
+    var query = document.getElementById('Main_menu');
+    if (query.classList.contains('display_none')) {
+        query.classList.remove('display_none'); 
+        query.classList.add('display_block'); 
+    } 
+    else{
+            query.classList.remove('display_block'); 
+            query.classList.add('display_none'); 
+    }
+};
+
+//----- Sub Sub Menu Show Hide
+[].slice.call(document.querySelectorAll('.toggleDetails')).forEach(function(e){e.onclick = function(){
+    this.parentElement.querySelector('.details').classList.toggle('hidden')
+}});
+
+//----Close (not use)
+function hide(){  
+    document.getElementById('closeDiv').style.display = 'none'; 
+};
+
+//-----Search Mobile
+function switchSearchVisible() {
+
+    var query = document.getElementById('Search_menu');
+    if (query.classList.contains('display_none')) {
+        query.classList.remove('display_none'); 
+        query.classList.add('display_block'); 
+    } 
+    else{
+            query.classList.remove('display_block'); 
+            query.classList.add('display_none'); 
+    }
+};
+function iconMenu() {
+    var query = document.getElementById('iconMenu');
+    if (query.classList.contains('fa-bars')) {
+        query.classList.remove('fa-bars'); 
+        query.classList.add('fa-xmark'); 
+    } 
+    else{
+            query.classList.remove('fa-xmark'); 
+            query.classList.add('fa-bars'); 
+    }
+};
+function iconSearch() {
+    var query = document.getElementById('iconSearch');
+    if (query.classList.contains('fa-magnifying-glass')) {
+        query.classList.remove('fa-magnifying-glass'); 
+        query.classList.add('fa-xmark'); 
+    } 
+    else{
+            query.classList.remove('fa-xmark'); 
+            query.classList.add('fa-magnifying-glass'); 
+    }
+};
+
+/*================/// Login And Signup Page ///==============*/
+const loginText = document.querySelector(".title-text .login");
+const loginForm = document.querySelector("form.login");
+const loginBtn = document.querySelector("label.login");
+const signupBtn = document.querySelector("label.signup");
+const signupLink = document.querySelector("form .signup-link a");
+signupBtn.onclick = (() => {
+    loginForm.style.marginLeft = "-50%";
+    loginText.style.marginLeft = "-50%";
+});
+    loginBtn.onclick = (() => {
+    loginForm.style.marginLeft = "0%";
+    loginText.style.marginLeft = "0%";
+});
+    signupLink.onclick = (() => {
+    signupBtn.click();
+    return false;
+});
 /*================///User Dashboar Sidebar page ///==============*/
 var lastState = false;
 
@@ -16,6 +112,21 @@ function userDashboardSidebarClicked() {
         document.getElementById("userSideButton_wrapper").style.display = "block";
     }
 }
+
+//---------Sidebar menu Show Hide----------
+$(document).ready(function () {
+    $(".accordion-heading").click(function () {
+        if ($(".accordion-body").is(':visible')) {
+            $(".accordion-body").slideUp(600);
+            $(".plusminus").text('+')
+        }
+        else {
+            $(this).next(".accordion-body").slideDown(600);
+            $(this).children(".plusminus").text('-');
+        }
+    });
+});
+
 /*================///Feedback page ///==============*/
 //feedback open or close
 var lastState = false;
